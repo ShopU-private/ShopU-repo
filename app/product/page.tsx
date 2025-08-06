b6c81c46787dc7c43db1aaa345071ee86dcfe3ae
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-08-06 23:33:34 +0530

    Update page.tsx

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -76,7 +76,7 @@ function ProductPageContent() {
               </div>
 
               {/* Products Grid */}
-              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
                 {currentProducts.map(product => (
                   <div key={product.id} className="flex justify-center">
                     <ProductCard
